1339d9bc247c4b866cb96c5b0aa14081ad414b0e
光騎 須永 <32146@ojs.ac.jp>
2021-05-27 14:08:01 +0900

    P63 リスト2.16

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
+            //List<Sale> sales = ReadSales("sales.csv");
         }
 
         //リスト2.15
