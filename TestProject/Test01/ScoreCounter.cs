92f87c50884ed491050d6cff7a30052a1e633014
光騎 須永 <32146@ojs.ac.jp>
2021-06-21 14:19:16 +0900

    Test 設問1完成

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,40 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
+            _score = ReadScore(filePath);
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要： 点数データを読み込み、studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            //生徒ごとの点数データを入れるリストオブジェクトを生成
+            List<Student> students = new List<Student>();
+            //ファイルから一気に読み込む
+            string[] lines = File.ReadAllLines(filePath);
+            //読み込んだ行の数だけ繰り返す
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                //Studentオブジェクトを生成
+                Student student = new Student {
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(student);
+            }
+            return students;
         }
 
-        //メソッドの概要： 
+        //メソッドの概要： 生徒別点数を求める
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
-            
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (var student in _score) {
+                if (dict.ContainsKey(student.Subject)) {
+                    dict[student.Subject] += student.Score;
+                } else {
+                    dict[student.Subject] = student.Score;
+                }
+            }
+            return dict;
         }
     }
 }
--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,10 @@
 ﻿namespace Test01 {
     class Student {
-
-
-
+        //学生の名前（読み込み専用）
+        public string Name { get; private set; }
+        //科目名
+        public string Subject { get; set; }
+        //点数
+        public int Score { get; set; }
     }
 }
