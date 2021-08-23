785507a5d1bb128d17646e6c1031dcba2485e145
須永光騎 <32146@ojs.ac.jp>
2021-08-23 15:54:16 +0900

    20210823べき乗

---
+++ Sample0823/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0823 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Exec_Click(object sender, EventArgs e) {
+            int ans = 1;
+            var num1 = int.Parse(Value.Text);
+            var num2 = int.Parse(Jyou.Text);
+
+            for (int i = 0; i < num2; i++) {
+                ans = ans * num1;
+            }
+
+            Result.Text = ans.ToString();
+        }
+    }
+}
